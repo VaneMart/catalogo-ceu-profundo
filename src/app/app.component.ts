42c70900a3f5bfcc1a8a6403e23f1937e7e590fc
VaneMart <vanessa.van.1@hotmail.com>
2022-11-14 22:44:50 -0300

    Revert "header, menu e footer"
    This reverts commit 5b42d837ad86d8ac2c40f708bffef8946bd6b485.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,42 +1,10 @@
-import * as $ from 'jquery';
-import * as M from 'materialize-css';
-import {
-  AfterViewInit,
-  Component,
-  ElementRef,
-  OnInit,
-  ViewChild,
-} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, AfterViewInit {
-  @ViewChild('mobile') sideNav? : ElementRef;
-
+export class AppComponent {
   title = 'catalogo-ceu-profundo';
-  isActive = true;
-  isAdmin = true;
-  coords: any;
-  currentDate: Date;
-
-  constructor() {
-    this.currentDate = new Date();
-  }
-
-  ngOnInit(): void {
-    navigator.geolocation.getCurrentPosition((position) => {
-      this.coords = position.coords;
-    });
-    setInterval(() => {
-      this.currentDate = new Date();
-    }, 1000);
-  }
-
-  ngAfterViewInit(): void {
-    let $sideNav = $('#side-nav');
-    M.Sidenav.init($sideNav);
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { MenuComponent } from './menu/menu.component';
-import { ContainerComponent } from './container/container.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    MenuComponent,
-    ContainerComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
