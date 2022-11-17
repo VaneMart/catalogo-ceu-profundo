40c86254fc0bbb497d2da00945b2d8fd4a9e4a53
VaneMart <vanessa.van.1@hotmail.com>
2022-11-16 23:36:17 -0300

    MENU - Interpolation | FOOTER - Property Biding

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { BrowserModule } from '@angular/platform-browser';
+
 
 @Component({
+  standalone: true,
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  styleUrls: ['./menu.component.css'],
+  imports: [
+    MatToolbarModule, 
+    MatIconModule,
+    BrowserModule]
 })
-export class MenuComponent {
+export class MenuComponent  {
 
   itens: string[] = ['Pesquisar', 'Catalogar'];
+  exibir: boolean = false;
 
-  constructor() { }
+  constructor() {}
+
+  exibirMenus() {
+    this.exibir = !this.exibir;
+  } 
 
 }
