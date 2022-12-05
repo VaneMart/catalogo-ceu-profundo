6d72d4fac14cb0365a0e0c0aae4d67a3b8b4b675
VaneMart <vanessa.van.1@hotmail.com>
2022-12-04 22:10:20 -0300

    atividade 12

--- src/app/service/anotacao/anotacao.service.ts
+++ src/app/service/anotacao/anotacao.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, Observable, throwError } from 'rxjs';
 import { Anotacao } from '../../model/anotacao.model';
 import { environment } from '../../../environments/environment';
 
@@ -12,17 +12,33 @@ export class AnotacaoService {
   constructor(private http: HttpClient) { }
 
   obter(uuid?: string): Observable<Anotacao[]> {
-    return this.http.get<Anotacao[]>(`${environment.apiUrl}/anotacoes?uuid=${uuid}&_sort=posicao&_order=asc`);
+    return this.http.get<Anotacao[]>(`${environment.apiUrl}/anotacoes?uuid=${uuid}&_sort=posicao&_order=asc`).pipe(
+      catchError(() => {
+        return throwError(() => new Error(`Ocorreu um erro ao tentar obter a anotação de uuid [${uuid}]`));
+      })
+    );
   }
 
   salvar(anotacao: Anotacao): Observable<Anotacao> {
     if (anotacao.id) {
-      return this.http.put<Anotacao>(`${environment.apiUrl}/anotacoes/${anotacao.id}`, anotacao);
+      return this.http.put<Anotacao>(`${environment.apiUrl}/anotacoes/${anotacao.id}`, anotacao).pipe(
+        catchError(() => {
+          return throwError(() => new Error(`Ocorreu um erro ao tentar atualizar a anotação de id [${anotacao.id}] e conteúdo [${anotacao.conteudo}]`));
+        })
+      );
     } 
-    return this.http.post<Anotacao>(`${environment.apiUrl}/anotacoes`, anotacao);
+    return this.http.post<Anotacao>(`${environment.apiUrl}/anotacoes`, anotacao).pipe(
+      catchError(() => {
+        return throwError(() => new Error(`Ocorreu um erro ao tentar inserir a anotação de conteúdo [${anotacao.conteudo}]`));
+      })
+    );
   }
 
   excluir(anotacao: Anotacao): Observable<Anotacao> {
-    return this.http.delete<Anotacao>(`${environment.apiUrl}/anotacoes/${anotacao.id}`);
+    return this.http.delete<Anotacao>(`${environment.apiUrl}/anotacoes/${anotacao.id}`).pipe(
+      catchError(() => {
+        return throwError(() => new Error(`Ocorreu um erro ao tentar excluir a anotação de id [${anotacao.id}] e conteúdo [${anotacao.conteudo}]`));
+      })
+    );
   }
 }
--- src/app/service/imagem/imagem.service.ts
+++ src/app/service/imagem/imagem.service.ts
@@ -1,8 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { Imagem } from '../../model/imagem.model';
+import { catchError, Observable, throwError } from 'rxjs';
 import { environment } from '../../../environments/environment';
+import { Imagem } from '../../model/imagem.model';
 
 @Injectable({
   providedIn: 'root'
@@ -11,18 +11,35 @@ export class ImagemService {
 
   constructor(private http: HttpClient) { }
 
+
   obter(uuid?: string): Observable<Imagem[]> {
-    return this.http.get<Imagem[]>(`${environment.apiUrl}/imagens?uuid=${uuid}&_sort=posicao&_order=asc`);
+    return this.http.get<Imagem[]>(`${environment.apiUrl}/imagens?uuid=${uuid}&_sort=posicao&_order=asc`).pipe(
+      catchError(() => {
+        return throwError(() => new Error(`Ocorreu um erro ao tentar obter a imagem de uuid [${uuid}]`));
+      })
+    );
   }
 
   salvar(imagem: Imagem): Observable<Imagem> {
     if (imagem.id) {
-      return this.http.put<Imagem>(`${environment.apiUrl}/imagens/${imagem.id}`, imagem);
+      return this.http.put<Imagem>(`${environment.apiUrl}/imagens/${imagem.id}`, imagem).pipe(
+        catchError(() => {
+          return throwError(() => new Error(`Ocorreu um erro ao tentar atualizar a imagem de id [${imagem.id}] e base64 [${imagem.base64}]`));
+        })
+      );
     } 
-    return this.http.post<Imagem>(`${environment.apiUrl}/imagens`, imagem);
+    return this.http.post<Imagem>(`${environment.apiUrl}/imagens`, imagem).pipe(
+      catchError(() => {
+        return throwError(() => new Error(`Ocorreu um erro ao tentar inserir a imagem de base64 [${imagem.base64}]`));
+      })
+    );
   }
 
   excluir(imagem: Imagem): Observable<Imagem> {
-    return this.http.delete<Imagem>(`${environment.apiUrl}/imagens/${imagem.id}`);
+    return this.http.delete<Imagem>(`${environment.apiUrl}/imagens/${imagem.id}`).pipe(
+      catchError(() => {
+        return throwError(() => new Error(`Ocorreu um erro ao tentar excluir a imagem de id [${imagem.id}] e base64 [${imagem.base64}]`));
+      })
+    );
   }
 }
